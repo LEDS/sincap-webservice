d10c449db79b42095381e7642ce5c0cb5ed8f2a7
Phillipe Lopes <phillipe.alcantara.fonseca@gmail.com>
2015-02-06 22:36:00 -0200

    Configuração da autenticação
    A autenticação agora é realizada pelo banco de dados
    do sincap.

--- src/main/groovy/br.ifes.leds.sincap.webservice/service/IndexService.groovy
+++ src/main/groovy/br.ifes.leds.sincap.webservice/service/IndexService.groovy
@@ -4,7 +4,6 @@ import br.ifes.leds.reuse.utility.Utility
 import br.ifes.leds.sincap.controleInterno.cln.cgt.AplCadastroInterno
 import br.ifes.leds.sincap.gerenciaNotificacao.cln.cgt.AplProcessoNotificacao
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
